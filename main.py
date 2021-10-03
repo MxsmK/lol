c2ed99abbb9c0aa6ce497967f9da03ab8b64a600
Mxmk <komuxin06@mail.ru>
2021-10-03 16:56:01 +0500

    Auto build APK file 32bit

--- main.py
+++ main.py
@@ -1,19 +1,115 @@
-from kivy.uix.screenmanager import Screen
+from kivy.lang import Builder
+from kivy.properties import ObjectProperty
 
 from kivymd.app import MDApp
-from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.boxlayout import MDBoxLayout
 
 
-class MainApp(MDApp):
+KV = '''
+<ContentNavigationDrawer>:
+
+    ScrollView:
+
+        MDList:
+
+            OneLineListItem:
+                text: "Список учеников"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "scr 1"
+
+            OneLineListItem:
+                text: "Добавить ученика"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "scr 2"
+            
+            OneLineListItem:
+                text: "Кто ест, а кто нет"
+                on_press:
+                    root.nav_drawer.set_state("close")
+                    root.screen_manager.current = "scr 3"
+
+
+MDScreen:
+
+    MDToolbar:
+        id: toolbar
+        pos_hint: {"top": 1}
+        elevation: 10
+        title: "Организация питания"
+        left_action_items: [["menu", lambda x: nav_drawer.set_state("open")]]
+
+    MDNavigationLayout:
+        x: toolbar.height
+
+        ScreenManager:
+            id: screen_manager
+
+            MDScreen:
+                name: "scr 1"
+                
+                MDLabel:
+                    text: "Тут список учеников"
+                    halign: "center"
+
+
+            MDScreen:
+                name: "scr 2"
+
+                MDLabel:
+                    text: "Тут функция добавления ученика"
+                    halign: "center"
+                
+                MDTextField:
+                    hint_text: "Имя"
+                    pos_hint: {"center_y": .45}
+                
+                MDTextField:
+                    hint_text: "Фамилия"
+                    pos_hint: {"center_y": .4}
+                
+                MDTextField:
+                    hint_text: "Счет"
+                    pos_hint: {"center_y": .35}
+                
+                MDFlatButton:
+                    id: button_create
+                    text: "Добавить"
+                    pos_hint: {"center_x": .5, "center_y": .30}
+                    
+                    
+            MDScreen:
+                name: "scr 3"
+
+                MDLabel:
+                    text: "Тут таблица кто ест, а кто нет"
+                    halign: "center"
+
+        MDNavigationDrawer:
+            id: nav_drawer
+
+            ContentNavigationDrawer:
+                screen_manager: screen_manager
+                nav_drawer: nav_drawer
+'''
+
+
+
+
+class ContentNavigationDrawer(MDBoxLayout):
+    screen_manager = ObjectProperty()
+    nav_drawer = ObjectProperty()
+
+
+class TestNavigationDrawer(MDApp):
+    def anything(self):
+        btn = self.screen.ids.button_create
+        if btn.on_press():
+            print("anything")
+
     def build(self):
-        screen = Screen()
-        screen.add_widget(
-            MDRectangleFlatButton(
-                text="Hello, World",
-                pos_hint={"center_x": 0.5, "center_y": 0.5},
-            )
-        )
-        return screen
+        return Builder.load_string(KV)
 
 
-MainApp().run()
+TestNavigationDrawer().run()
